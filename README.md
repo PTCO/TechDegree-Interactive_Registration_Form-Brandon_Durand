a123dbe0d696584bd510c44986dd47b44381e551
Brandon Durand <ptco.dev@gmail.com>
2024-03-14 01:58:25 -0500

    final version

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # TechDegree-Interactive_Registration_Form-Brandon_Durand
- TechDegree Project 7
+
+TechDegree Project 7
+
+Real Time Email Check
+
+Email validation runs while user enters an email address;
+
+Valid email address requirements:
+
+1. Must contain a .com domain
+2. A valid email service such as gmail or outlook
+3. Must contain an '@'
+4. Must not contain any special characters ie, '$ , &, %, #', etcetera
+
+Conditional Error Message
+
+Credit card nubmer validation will display two different error messages:
+
+1. 'Credit card number cannot be empty or blank' if field is blank or empty
+2. 'Credit card number must be between 13 - 16 digits & contain no special characters or letters' if Card number is not between 13-16 digits or special characters or letters are entered
--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 const otherJobField = document.querySelector('#other-job-role');
 const shirtColorField = document.querySelector('#color')
+const activities = document.querySelectorAll('#activities-box > label > input');
 const jobRoles = document.querySelector('#title');
 let Colors = shirtColorField.querySelectorAll('option');
 
 // Page Load
 window.onload = ()=>{
     document.querySelector('#name').focus();
-    document.querySelector("option[value='credit-card'] ").setAttribute('selected', '')
+    document.querySelector("option[value='credit-card'] ").setAttribute('selected', '');
+    activities.forEach((input)=> { input.checked = false })
     otherJobField.style.display = 'none';
     shirtColorField.setAttribute('disabled', "")
     showMethod('credit-card')
@@ -58,21 +60,50 @@ document.querySelector('#design').addEventListener('click', e=> {
 })
 
 // Activities Total
-const activities = document.querySelectorAll('#activities-box > label > input');
 const activitiesCost = document.querySelectorAll('#activities-box > label > .activity-cost');
+const activitiesTime = document.querySelectorAll('#activities-box > label > span');
 const activityTotal = document.querySelector('#activities-cost');
 
-let cost = 0;
+function disableActivities(selectedActivity){
 
+    let selectedTime = selectedActivity.nextElementSibling.nextElementSibling.innerText
+
+    activitiesTime.forEach((activity)=> {
+
+        let sameTime = activity.innerHTML;
+
+        if(sameTime === selectedTime){
+            activity.previousElementSibling.previousElementSibling.setAttribute('disabled', "")
+            selectedActivity.removeAttribute('disabled');
+            activity.parentElement.className = 'disabled';
+        }
+    })
+}
+function enableActivities(){
+    activitiesTime.forEach((activity)=> {
+        activity.parentElement.className = ' ';
+        activity.previousElementSibling.removeAttribute('disabled')
+    })
+}
+
+let cost = 0;
 activities.forEach((input, index)=> {
     input.addEventListener('click', e => {
         if(input.checked === true) {
+            disableActivities(input);
             cost += parseInt(activitiesCost[index].innerText.replace('$', ' '));
         }
         else if(input.checked === false){
+            enableActivities()
             cost -= parseInt(activitiesCost[index].innerText.replace('$', ' '));
         }
-        activityTotal.innerText = `$ ${cost}`
+        activityTotal.innerText = `Total: $${cost}`
+    })
+    input.addEventListener('focus', ()=>{
+        input.parentElement.className = 'focus';
+        input.addEventListener('blur', ()=>{
+                input.parentElement.className = ' ';
+        })
     })
 })
 
@@ -104,30 +135,102 @@ paymentMethods.addEventListener('change', e=> {
 // Form Validation
 
 const form = document.querySelector('form');
+const inputs = form.querySelectorAll('input[id]');
+const payment = document.querySelector('#payment');
+
+// Regular Expressions
+const validEmail = /^[a-zA-Z0-9.]*@\w+.com$/gm;
+const validCreditCardNum = /^\d{13,16}$/gm;
+const validZip = /^\d{5}$/gm;
+const validCVV = /^\d{3}$/gm;
+
+let errors = 0;
+
+function showInvalid(input , classToKeep = '', hintMessage = ''){
+    input.parentElement.className = `${classToKeep} not-valid`
+    input.parentElement.lastElementChild.style.display = 'block'
+
+    if(hintMessage !== ''){
+        input.parentElement.lastElementChild.innerText = hintMessage;
+    }
+
+    errors++;
+}
+
+function showValid(input, classToKeep = ''){
+    input.parentElement.className = `${classToKeep} valid`
+    input.parentElement.lastElementChild.style.display = 'none'
+}
 
 form.addEventListener('submit', e=> {
-    e.preventDefault();
-    const inputs = form.querySelectorAll('input[id]');
 
-    // Regular Expressions
-    const validEmail = /^[a-zA-Z0-9.]*@\w+.[a-z0-9.][a-z0-9.][a-z0-9.]$/gm;
+    // Activities Check
+    if(activityTotal.innerText === 'Total: $0'){
+        showInvalid(activityTotal, 'activities')
+    }
+    else{
+        showValid(activityTotal, 'activities')
+    }
 
-    // Checks
+    // Validation
     inputs.forEach((input)=> {
+                   
+        // Basic Info
         if(input.id === 'name' && input.value === '') {
-            input.parentElement.className = 'not-valid'
-            input.parentElement.lastElementChild.style.display = 'block'
+            showInvalid(input)
+        }
+        else if(input.id === 'name' && input.value !== ''){
+            showValid(input)
         }
         else if(input.id === 'email' && !validEmail.test(input.value)) {
-            input.parentElement.className = 'not-valid'
-            input.parentElement.lastElementChild.style.display = 'block'
+            showInvalid(input)
+        }
+        else if(input.id === 'email'){
+            showValid(input)
+        }
+
+        // Payment Methods
+        else if(payment.value === 'credit-card' && input.id === 'cc-num' && !validCreditCardNum.test(input.value) && input.value !== ''){
+            showInvalid(input, '', 'Credit card number must be between 13 - 16 digits & contain no special characters or letters')
+        }
+        else if(payment.value === 'credit-card' && input.id === 'cc-num' && input.value === ''){
+            showInvalid(input, '', 'Credit card number cannot be empty or blank')
+        }
+        else if(payment.value === 'credit-card' && input.id === 'cc-num'){
+            showValid(input)
+        }
+        else if(payment.value === 'credit-card' && input.id === 'zip' && !validZip.test(input.value)){
+            showInvalid(input)
+        }
+        else if(payment.value === 'credit-card' && input.id === 'zip'){
+            showValid(input)
+        }
+        else if(payment.value === 'credit-card' && input.id === 'cvv' && !validCVV.test(input.value)){
+            showInvalid(input)
+        }
+        else if(payment.value === 'credit-card' && input.id === 'cvv'){
+            showValid(input)
         }
     })
 
+    if(errors > 0){
+        e.preventDefault();
+    }
 
 })
 
-
+inputs.forEach((input)=> {
+    if(input.id === 'email'){
+        input.addEventListener('keyup', ()=> {
+            if(!validEmail.test(input.value)) {
+                showInvalid(input)
+            }
+            else{
+                showValid(input)
+            }
+        })
+    }
+})
 
 
 
